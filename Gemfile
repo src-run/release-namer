4c0699ed3b61507694afc92916a621bf51992eee
Rob Frawley 2nd <rmf@src.run>
2018-09-11 13:52:18 -0400

    updated to support custom formats using pos types
    Signed-off-by: Rob Frawley 2nd <rmf@src.run>

--- Gemfile
+++ Gemfile
@@ -1,5 +1,4 @@
 source 'https://rubygems.org'
 
-gem 'rbtagger'
-gem 'open_uri_redirections'
+gem 'engtagger'
 gem 'nokogiri'
--- namer.rb
+++ namer.rb
@@ -8,137 +8,198 @@
 ##
 
 require 'optparse'
-require 'rbtagger'
+require 'engtagger'
+require 'uri'
 require 'open-uri'
-require 'open_uri_redirections'
 require 'nokogiri'
 
 class ReleaseNamer
 
-  def initialize(sites, proper = nil)
-    @proper = proper
-    @text   = String.new
-    for site in sites
-      source = open(site, :allow_redirections => :safe) { |f| f.read }
-      dom = Nokogiri.HTML(source)
-      dom.css('head,script,style,code').map { |l| l.unlink }
-      @text += dom.text
-    end
+  VERSION = '1.2.0'
+  LICENSE = 'MIT'
+  AUTHORS = [
+      'Rob Frawley 2nd <rmf@src.run>',
+      'Dan Corrigan <dcorrigan@scribenet.com>'
+  ]
+
+  TAG_METHOD_MAP = {
+    get_proper_nouns: %w(NNP),
+    get_nouns: %w(NN),
+    get_verbs: %w(VB VBD VBG PART VBP VBZ),
+    get_infinitive_verbs: %w(VB),
+    get_past_tense_verbs: %w(VBD),
+    get_gerund_verbs: %w(VBG),
+    get_passive_verbs: %(PART),
+    get_base_present_verbs: %w(VBP),
+    get_present_verbs: %w(VBZ),
+    get_adjectives: %w(JJ),
+    get_comparative_adjectives: %w(JJR),
+    get_superlative_adjectives: %w(JJS),
+    get_adverbs: %w(RB RBR RBS RP),
+    get_interrogatives: %w(WRB WDT WP WPS),
+    get_conjunctions: %w(CC IN)
+  }
+
+  def initialize(uris, format)0
+    @words = (uris.map { |u| get_tagger.add_tags(uri_to_txt(u)) }).join(' ')
+    @calls = get_pos_methods format
   end
 
   def suggestion
-    adj = pick_word(adjectives)
-    noun = pick_word(nouns)
-    "#{adj}_#{noun}"
+    @calls.map { |c| pick_word(get_word_list(c)) }.join('-')
   end
 
   private
 
-  def cleanup_word(w)
-    w.downcase!
-    w.gsub!(/[\.,:;!\?=\\`\{\}\[\]\+\-\(\)" ]/, '')
+  def get_pos_methods(types)
+    types.map { |t| resolve_tag_method t }.reject{ |m| m == nil }
   end
 
-  def get_simple_list(tag_method)
-    arr = tagger.send(tag_method, @text).map { |x, l| x }.uniq
-    remove_bs(arr)
-    arr
+  def resolve_tag_method(tag)
+    for i, v in TAG_METHOD_MAP
+      if v.include?(tag)
+        return i.to_s
+      end
+    end
+
+    nil
   end
 
-  def get_subtler_list(tag_method, screeners = [])
-    arr = tagger.send(tag_method, @text)
-    arr.reject! { |x,t| !screeners.include?(t) }
-    arr.map! { |x, l| x }.uniq
-    remove_bs(arr)
-    arr
+  def uri_to_txt(uri)
+    src_to_dom(open(uri, "User-Agent" => "Ruby/#{RUBY_VERSION}") { |f| f.read }).text
   end
 
-  BS = ['[0-9]', '\/']
-
-  def bs_patterns
-    BS.join('|')
+  def src_to_dom(src)
+    dom = Nokogiri.HTML(src)
+    dom.css('head,script,style,code').map { |l|
+      l.unlink
+    }
+    dom
   end
 
-  def remove_bs(arr)
-    arr.reject! { |x| x.match(bs_patterns) }
+  def get_word_list(type)
+    get_tagger.send(type, @words).map { |i, v| String.new(i) }
   end
 
-  def tagger
-    @tagger ||= Brill::Tagger.new
+  def get_tagger
+    @tagger ||= EngTagger.new
   end
 
-  def pick_word(arr)
-    pick = random_pick(arr)
-    cleanup_word(pick)
-    pick
+  def pick_word(words)
+    cleanup_word(pick_random_word(cleanup_word_list(words)))
   end
 
-  def random_pick(arr)
-    arr[rand(arr.size - 1)]
+  def pick_random_word(words)
+    words[rand(words.size - 1)]
   end
 
-  def adjectives
-    @adjectives ||= get_simple_list(:adjectives)
+  def cleanup_word_list(list)
+    list.reject { |x| x.match('[^a-zA-Z-]') }
   end
 
-  def get_noun_list
-    @proper ? get_subtler_list(:nouns, %w(NNP NNPS)) : get_simple_list(:nouns)
-  end
-
-  def nouns
-    @nouns ||= get_noun_list
+  def cleanup_word(word)
+    word.downcase
   end
 
 end
 
-def main
+class ReleaseNamerCliConfig
 
-  opts = {}
-  optparse = OptionParser.new do|o|
-    o.banner = "Usage:\n"+
-      "  release-namer [options] [--] [<url-resources>]...\n\n"+
-      "Example:\n"+
-      "  release-namer --count=20 -P \"https://en.wikipedia.org/wiki/Computer_science\" \"https://en.wikipedia.org/wiki/Art\"\n\n"+
-      "Options:"
+  DEFAULT_URIS = [
+      'http://en.wikipedia.org/wiki/Special:Random',
+  ]
 
-    opts[:count] = 1
-    o.on('-c', '--count INT', 'Number of results to return.') do|count|
-      opts[:count] = count.to_i
+  def initialize
+    options = {
+        :count => 1,
+        :format => Array.new,
+        :uris => Array.new,
+    }
+
+    parser = OptionParser.new do|o|
+      o.banner =
+          "Usage:\n"+
+          "    release-namer [options] [--] [<url-1> <url-2> ...]\n\n"+
+          "Example:\n"+
+          "    release-namer --count=5 --format=JJ --format=VBD --format=NN\n"+
+          "    release-namer --count=9 'https://en.wikipedia.org/wiki/Computer_science' 'https://en.wikipedia.org/wiki/Art'\n\n"+
+          "Arguments:\n"+
+          "    Any number of urls can be provided as arguments, the source text of which will be used as the pool of\n"+
+          "    random words for the generator. This allows you to specify urls with content that is targeted within\n"+
+          "    the \"focus\" you'd like the generator to draw from. When specifying Wikipedia pages, instead of the\n"+
+          "    complete url, you may specify only the Wikipedia page name. If no url arguments are provided the default\n"+
+          "    url(s) used are: #{DEFAULT_URIS}\n\n"+
+          "Options:"
+
+      o.on('-c', '--count INT', 'Number of generated name results to create and output.') do |count|
+        options[:count] = count.to_i
+      end
+
+      o.on('-f', '--format STR', 'Define a custom format for the generated names using parts of speech tags.') do |format|
+        options[:format].push format
+      end
+
+      o.on('-h', '--help', 'Display this help message.') do
+        puts o
+        exit 255
+      end
+
+      o.on('-t', '--help-tags', 'Display the supported parts of speech tags for the "--format" option.') do
+        write_tag_list
+        exit 255
+      end
+
+      o.on('-v', '--version', 'Show script version string.') do
+        printf "Project Release Namer v%s (%s License) [%s]\n", ReleaseNamer::VERSION, ReleaseNamer::LICENSE, ReleaseNamer::AUTHORS.join(', ')
+        exit 0
+      end
     end
 
-    opts[:proper] = false
-    o.on('-P', '--proper', 'Include a proper noun in results.') do
-      opts[:proper] = true
-    end
+    parser.parse!
 
-    o.on('-h', '--help', 'Display this help message.') do
-      puts o
-      exit 255
-    end
+    options[:uris] = parse_uris(ARGV)
+    options[:uris] = parse_uris(DEFAULT_URIS) if options[:uris].length == 0
 
-    o.on('-v', '--version', 'Show script version string.') do
-      puts "release-namer v0.1.0"
-      exit 0
+    options[:count].times do |_x|
+      puts get_release_namer(options).suggestion
     end
   end
 
-  optparse.parse!
-
-  opts[:sites] = Array.new
-  ARGV.each do|s|
-    opts[:sites] << s
+  def get_release_namer(options)
+    @namer ||= ReleaseNamer.new(options[:uris], options[:format].length == 0 ? %w(JJ NN) : options[:format])
   end
 
-  opts[:sites] << 'http://en.wikipedia.org/wiki/Special:Random' if opts[:sites].length == 0
+  def write_tag_list
+    printf "TAG\tDESCRIPTION\n---\t-----------\n"
+    EngTagger::TAGS.each { |t, d| write_tag_list_item(t) }
+  end
 
-  namer = ReleaseNamer.new(opts[:sites], opts[:proper])
+  def write_tag_list_item(tag)
+    printf "%s\t%s\n", tag.upcase, clean_and_format_tag_desc(EngTagger::explain_tag(tag))
+  end
 
-  opts[:count].times do |_x|
-    puts namer.suggestion
+  def clean_and_format_tag_desc(desc)
+    desc.split('_').collect(&:capitalize).join(' ')
+  end
+
+  def parse_uris(uris)
+    uris.map{ |l| parse_uri_from_str(l) }
+  end
+
+  def parse_uri_from_str(str)
+    uri = create_uri(str)
+    uri = create_uri(sprintf('http://en.wikipedia.org/wiki/%s', str)) unless uri.kind_of?(URI::HTTP) || uri.kind_of?(URI::HTTPS)
+    uri
+  end
+
+  def create_uri(uri)
+    URI.parse(uri) rescue false
   end
 
 end
 
-main
+ReleaseNamerCliConfig.new do |_n|
+  puts _n.suggestion
+end
 
 ## EOF
